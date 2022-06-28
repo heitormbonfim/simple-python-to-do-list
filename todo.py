22279796d63357dd7d1f0185c1ca637e19070ea3
heitorthewizard <heitor.mb27@gmail.com>
2022-06-28 15:26:37 -0300

    project done

--- todo.py
+++ todo.py
@@ -1,9 +1,12 @@
 from tkinter import *
+from tkinter import filedialog
 from tkinter.font import Font
+from tkinter.filedialog import FileDialog
+import pickle
 
 root = Tk()
 root.title('To Do List')
-#root.iconbitmap('')
+# root.iconbitmap('')
 root.geometry('500x500')
 
 
@@ -17,16 +20,16 @@ frame = Frame(root)
 frame.pack()
 
 list_box = Listbox(frame,
-    font=font,
-    width=25,
-    height=5,
-    background="#f0f0f0",
-    border=0,
-    foreground='#262626',
-    highlightthickness=0,
-    selectbackground='#a6a6a6',
-    activestyle='none',
-)
+                   font=font,
+                   width=25,
+                   height=5,
+                   background="#f0f0f0",
+                   border=0,
+                   foreground='#262626',
+                   highlightthickness=0,
+                   selectbackground='#a6a6a6',
+                   activestyle='none',
+                   )
 
 list_box.pack(side=RIGHT, fill=BOTH)
 
@@ -37,36 +40,108 @@ list_box.config(yscrollcommand=scrolbar.set)
 scrolbar.config(command=list_box.yview)
 
 
-entry_box = Entry(root, font=('Helvenica', 24))
+entry_box = Entry(root, font=('Helvenica', 24), width=24)
 entry_box.pack(pady=20)
 
 frame_button = Frame(root)
 frame_button.pack(pady=20)
 
-def add_item():
+
+def add_task():
     list_box.insert(END, entry_box.get())
     entry_box.delete(0, END)
 
-def delete_item():
+
+def delete_task():
     list_box.delete(ANCHOR)
 
-def cross_off_item():
-    pass
 
-def uncross_item():
-    pass
+def cross_off_task():
+    list_box.itemconfig(
+        list_box.curselection(),
+        foreground='#cccccc',
+    )
+    # get rid of selection bar
+    list_box.selection_clear(0, END)
 
 
-add_button = Button(frame_button, text='Add Item', command=add_item)
-delete_button = Button(frame_button, text='Delete Item', command=delete_item)
-cross_off_button = Button(frame_button, text='Cross Off', command=cross_off_item)
-uncross_button = Button(frame_button, text='Uncross Off', command=uncross_item)
+def uncross_task():
+    list_box.itemconfig(
+        list_box.curselection(),
+        foreground='#262626',
+    )
+    list_box.selection_clear(0, END)
 
-add_button.grid(row=0, column=0, padx=20)
+
+def delete_done_tasks():
+    counter = 0
+    while counter < list_box.size():
+        if list_box.itemcget(counter, 'foreground') == '#cccccc':
+            list_box.delete(list_box.index(counter))
+        else:
+            counter += 1
+
+
+def save_list():
+    file_name = filedialog.asksaveasfilename(
+        initialdir='/home/heitorthewizard/Documents/studies/python/tkinter/python-todo-list',
+        title='Save File',
+        filetypes=(('Dat Files', '*.dat'), ('All Files', '*.*'))
+    )
+    if file_name:
+        delete_done_tasks()
+        if file_name.endswith('.dat'):
+            pass
+        else:
+            file_name = f'{file_name}.dat'
+
+        tasks = list_box.get(0, END)
+        output_file = open(file_name, 'wb')
+        pickle.dump(tasks, output_file)
+
+
+def open_list():
+    file_name = filedialog.askopenfilename(
+        initialdir='/home/heitorthewizard/Documents/studies/python/tkinter/python-todo-list',
+        title='Open File',
+        filetypes=(('Dat Files', '*.dat'), ('All Files', '*.*'))
+    )
+
+    if file_name:
+        list_box.delete(0, END)
+        input_file = open(file_name, 'rb')
+        tasks = pickle.load(input_file)
+        for task in tasks:
+            list_box.insert(END, task)
+
+
+def clear_list():
+    list_box.delete(0, END)
+
+
+menu_bar = Menu(root)
+root.config(menu=menu_bar)
+
+file_menu = Menu(menu_bar, tearoff=False)
+menu_bar.add_cascade(label='File', menu=file_menu)
+
+file_menu.add_command(label='Save List', command=save_list)
+file_menu.add_command(label='Open List', command=open_list)
+file_menu.add_separator()
+file_menu.add_command(label='Clear List', command=clear_list)
+
+
+add_button = Button(frame_button, text='Add Task', command=add_task)
+delete_button = Button(frame_button, text='Delete Task', command=delete_task)
+cross_off_button = Button(frame_button, text='Done', command=cross_off_task)
+uncross_button = Button(frame_button, text='Undo', command=uncross_task)
+delete_done_tasks_button = Button(
+    frame_button, text='Delete Done Tasks', command=delete_done_tasks)
+
+add_button.grid(row=0, column=0)
 delete_button.grid(row=0, column=1)
 cross_off_button.grid(row=0, column=2)
-uncross_button.grid(row=0, column=3, padx=20)
-
-
+uncross_button.grid(row=0, column=3)
+delete_done_tasks_button.grid(row=0, column=4)
 
 root.mainloop()
